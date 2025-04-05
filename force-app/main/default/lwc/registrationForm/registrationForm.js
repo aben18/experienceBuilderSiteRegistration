594a4cbeae151f71f90e7ddeb9820ff44a59005c
Brian Bell <iambrianbell@gmail.com>
2025-04-05 13:19:38 -0500

    feat: Add initial code from ai

--- force-app/main/default/lwc/registrationForm/registrationForm.js
+++ force-app/main/default/lwc/registrationForm/registrationForm.js
@@ -1,3 +1,51 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import findMatchingContact from '@salesforce/apex/RegistrationController.findMatchingContact';
+import submitRegistration from '@salesforce/apex/RegistrationController.submitRegistration';
 
-export default class RegistrationForm extends LightningElement {}
+export default class RegistrationForm extends LightningElement {
+  @track firstName = '';
+  @track lastName = '';
+  @track email = '';
+  @track matchedAccount = '';
+  @track showNewAccountField = false;
+  @track newAccountName = '';
+
+  handleInput(event) {
+    this[event.target.name] = event.target.value;
+  }
+
+  async findContactMatch() {
+    try {
+      const result = await findMatchingContact({
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email
+      });
+
+      if (result && result.AccountName) {
+        this.matchedAccount = result.AccountName;
+        this.showNewAccountField = false;
+      } else {
+        this.matchedAccount = '';
+        this.showNewAccountField = true;
+      }
+    } catch (error) {
+      console.error('Contact match error:', error);
+    }
+  }
+
+  async handleSubmit() {
+    try {
+      const result = await submitRegistration({
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        accountName: this.matchedAccount || this.newAccountName
+      });
+
+      console.log('Registration success:', result);
+    } catch (error) {
+      console.error('Submit error:', error);
+    }
+  }
+}
