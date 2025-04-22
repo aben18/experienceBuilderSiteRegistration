e91bd352f975f8e98ef93e5e1a13ea2a0938c30e
Brian Bell <iambrianbell@gmail.com>
2025-04-21 20:24:29 -0500

    test: Add modal module and fix test file

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@ const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
     ...jestConfig,
-    modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
+    modulePathIgnorePatterns: ['<rootDir>/.localdevserver'],
+    moduleNameMapper: {
+        "^lightning/modal$": "<rootDir>/force-app/test/jest-mocks/lightning/modal"
+    }
 };
